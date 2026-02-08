cf5146e2c5ad8a95196f5c1fed5d0657f312a277
Alexander Ukwueze <contact@mail.lexiz.is-a.dev>
2026-02-08 13:04:29 +0000

    fix: sitemap generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,17 +7,15 @@ interface StorySitemapItem {
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const NEXT_PUBLIC_APP_URL = "https://x-inkflow.vercel.app";
-  // process.env.NEXT_PUBLIC_APP_URL || ;
+  const NEXT_PUBLIC_APP_URL =
+    process.env.NEXT_PUBLIC_APP_URL || "https://x-inkflow.vercel.app";
 
   let storyEntries: MetadataRoute.Sitemap = [];
 
   try {
-    const { data } = await api.get("/stories/all-slugs", {
+    const { data } = await api.get("/api/stories/all-slugs", {
       baseURL: NEXT_PUBLIC_APP_URL,
     });
-
-    console.log(data);
 
     storyEntries = data.data.map((story: StorySitemapItem) => ({
       url: `${NEXT_PUBLIC_APP_URL}/stories/${story.slug}`,
